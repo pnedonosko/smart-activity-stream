59686a64a3c6f69dfefcf557b8c5b42461ab151c
Vlad Martyshevskyi <vladmartishevskii@gmail.com>
2018-10-30 15:47:10 +0200

    Formatting

--- webapp/src/main/java/org/exoplatform/datacollector/portlet/DataCollectorPortlet.java
+++ webapp/src/main/java/org/exoplatform/datacollector/portlet/DataCollectorPortlet.java
@@ -9,13 +9,13 @@ import org.exoplatform.webui.application.WebuiRequestContext;
 
 public class DataCollectorPortlet extends GenericPortlet {
 
-	/**
-	 * Renders the portlet on the page.
-	 */
-	@Override
-	protected void doView(final RenderRequest request, final RenderResponse response) {
-		JavascriptManager js = ((WebuiRequestContext) WebuiRequestContext.getCurrentInstance()).getJavascriptManager();
-		js.require("SHARED/datacollector", "datacollector");
-	}
+  /**
+   * Renders the portlet on the page.
+   */
+  @Override
+  protected void doView(final RenderRequest request, final RenderResponse response) {
+    JavascriptManager js = ((WebuiRequestContext) WebuiRequestContext.getCurrentInstance()).getJavascriptManager();
+    js.require("SHARED/datacollector", "datacollector");
+  }
 
 }
