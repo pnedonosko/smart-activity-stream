c3604b5e33d0bf532bd028fa11177ff916f56129
Nick Riabovol <nick.riabovol@gmail.com>
2019-10-17 01:14:13 +0300

    Add Activity Statistics manager in Administration menu

---
+++ webapp/src/main/java/org/exoplatform/smartactivitystream/admin/portlet/SmartActivityStreamAdminPortlet.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2003-2017 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.exoplatform.smartactivitystream.admin.portlet;
+
+//import static org.exoplatform.webconferencing.Utils.getResourceMessages;
+
+import java.io.IOException;
+
+import javax.portlet.*;
+
+import org.exoplatform.container.ExoContainer;
+import org.exoplatform.container.ExoContainerContext;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+import org.exoplatform.web.application.JavascriptManager;
+import org.exoplatform.webui.application.WebuiRequestContext;
+
+//import org.exoplatform.webconferencing.UserInfo;
+//import org.exoplatform.webconferencing.WebConferencingService;
+
+/**
+ * Created by The eXo Platform SAS
+ * 
+ * @author <a href="mailto:nik.riabovol@gmail.com">Nikita Riabovol</a>
+ * @version $Id: SmartActivityStreamAdminPortlet.java 00000 Oct 15, 2019 NickEngineer
+ *          $
+ */
+public class SmartActivityStreamAdminPortlet extends GenericPortlet {
+
+  /** The Constant LOG. */
+  private static final Log LOG = ExoLogger.getLogger(SmartActivityStreamAdminPortlet.class);
+
+  /** The Web Conferencing service. */
+  // private WebConferencingService webConferencing;
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void init() throws PortletException {
+    super.init();
+
+    ExoContainer container = ExoContainerContext.getCurrentContainer();
+    // this.webConferencing =
+    // container.getComponentInstanceOfType(WebConferencingService.class);
+  }
+
+  /**
+   * Admin view.
+   *
+   * @param request the request
+   * @param response the response
+   * @throws IOException Signals that an I/O exception has occurred.
+   * @throws PortletException the portlet exception
+   */
+  @Override
+  public void doView(RenderRequest request, RenderResponse response) throws IOException, PortletException {
+    final String remoteUser = request.getRemoteUser();
+    try {
+
+      PortletRequestDispatcher prDispatcher = getPortletContext().getRequestDispatcher("/WEB-INF/pages/admin.jsp");
+      prDispatcher.include(request, response);
+
+      // Javascript
+      /*JavascriptManager js = ((WebuiRequestContext) WebuiRequestContext.getCurrentInstance()).getJavascriptManager();
+      js.require("SHARED/webConferencingAdminPortlet", "webConferencingAdminPortlet")
+        .addScripts("webConferencingAdminPortlet.init();"); */// messages: " + asJSON(messages) + " - i18n not yet required by the
+                                                            // script
+
+    } catch (Exception e) {
+      LOG.error("Error processing Smart Activity Stream Admin portlet for user " + remoteUser, e);
+    }
+  }
+
+}
---
+++ webapp/src/main/webapp/WEB-INF/pages/admin.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+
+<%@ page import="java.util.Map" %>
+
+<%
+    //Map<String, String> messages = (Map<String, String>) request.getAttribute("messages");
+%>
+
+<div id="webconferencingAdmin" class="container-fluid">
+    <h3 class="titleWithBorder">Test</h3>
+    <div class="content">
+        <p>Hello smartactivitystream portlet admin page =)</p>
+
+    </div>
+</div>
